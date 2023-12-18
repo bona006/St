7a932dc847f5340a880d2c79e817be52d5d9b457
Ilya <bona-006@mail.ru>
2023-12-18 19:19:23 +0300

    done

---
+++ Z2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = AckermannFunction(m, n);
+Console.WriteLine($"Ackermann({m}, {n}) = {result}");
+     int AckermannFunction(int M, int N)
+    {
+        if (M == 0)
+        {
+            return N + 1;
+        }
+        else if (N == 0)
+        {
+            return AckermannFunction(M - 1, 1);
+        }
+        else
+        {
+            return AckermannFunction(M- 1, AckermannFunction(M, N - 1));
+        }
+    }
---
+++ Z3/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = { 1, 2, 3, 4, 5 };
+PrintArrayReversed(array, array.Length - 1);
+
+static void PrintArrayReversed(int[] array, int index)
+{
+    if (index >= 0)
+    {
+        Console.WriteLine(array[index]);
+        PrintArrayReversed(array, index - 1);
+    }
+}
