714330290983062d9501fbb27cab83f2f09180a5
Ilya <bona-006@mail.ru>
2023-12-18 18:38:51 +0300

    z1

---
+++ Z1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+NaturalNumbers(m,n);
+void NaturalNumbers(int M, int N)
+{
+    if(M<=N)
+    {
+        Console.Write(M+", ");
+        NaturalNumbers(M+1,N);
+    }
+}
